0e61f4a5aeb1dacce994fe82c08c2f614e90551c
Rukeshr <rukeshr16@gmail.com>
2023-03-20 18:56:38 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,11 @@ function Header(){
     return (
         <header>
             <nav className="nav">
-            <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo" />
                 <h1>React</h1>
                 <ul className="nav-items">
-                   <a href="https://react.dev" className="nav-items"><li>More Info</li></a>
+                <a href="https://github.com/Rukeshr/My-projects-RukeshR/blob/master/JavaScript/JavaScript.txt" className="nav-items"><li>JS Notes</li></a> 
+                <a href="https://react.dev" className="nav-items"><li>More Info</li></a>  
                 </ul>
             </nav>
         </header>
