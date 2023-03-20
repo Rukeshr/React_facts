0f865a6e7ece7f991d53546685a71bb3d6aea2ec
Rukeshr <rukeshr16@gmail.com>
2023-03-20 19:06:18 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Page from './components/Page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,5 +9,3 @@ root.render(
     <Page />
   </React.StrictMode>
 );
-
-reportWebVitals();
